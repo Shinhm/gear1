3e90a5efcaf206f9bb76ecf0757269e745e2c9b1
hmmm <witshm@gmail.com>
2019-02-27 00:01:22 +0900

    btv 베스트 영화 다시보기 수정

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,62 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const async = require('async');
 const cheerio = require('cheerio');
 const request = require('request');
 const Iconv = require('iconv').Iconv;
 const iconv = new Iconv('CP949', 'utf-8//translit//ignore');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
 router.get("/getResource", function (req, res, next) {
-  let resultArr = {
-    btv: {
-      movie: [],
-      etc: [],
-    },
-    ollehtv: {
-      movie: [],
-      etc: [],
-    }
-  };
-  let sktMovieVodUrl = "http://skbroadband.com/content/vod/Vod_List.do?depth1_menu=105";
-  let sktEtcVodUrl = "http://skbroadband.com/content/vod/Vod_List.do?depth1_menu=105";
+    let resultArr = {
+        btv: {
+            movie: [],
+            etc: [],
+        },
+        ollehtv: {
+            movie: [],
+            etc: [],
+        }
+    };
+    let sktMovieVodUrl = "http://skbroadband.com/content/vod/Vod_List.do?depth1_menu=105";
+    let sktEtcVodUrl = "http://skbroadband.com/content/vod/Vod_List.do?depth1_menu=20100";
 
-  request({ url: sktMovieVodUrl, encoding: null }, function (error, response, body) {
-    let htmlDoc = iconv.convert(body).toString();
-    const $ = cheerio.load(htmlDoc);
-    let colArr = $(".movie_front");
-    for (let i = 0; i < colArr.length; i++) {
-      resultArr.btv.movie.push({ src: colArr[i].children[3].attribs.src, title: colArr[i].children[3].attribs.alt});
-    }
-
-    res.json(resultArr)
-  });
-})
+    async.waterfall([
+        function(callback) {
+            request({url: sktEtcVodUrl, encoding: null}, function (error, response, body) {
+                let htmlDoc = iconv.convert(body).toString();
+                const $ = cheerio.load(htmlDoc);
+                let colArr = $(".movie_front");
+                for (let i = 0; i < colArr.length; i++) {
+                    resultArr.btv.etc.push({
+                        src: colArr[i].children[3].attribs.src,
+                        title: colArr[i].children[3].attribs.alt
+                    });
+                }
+                callback(error, resultArr);
+            });
+        },
+        function(resultArr, callback) {
+            request({url: sktMovieVodUrl, encoding: null}, function (error, response, body) {
+                let htmlDoc = iconv.convert(body).toString();
+                const $ = cheerio.load(htmlDoc);
+                let colArr = $(".movie_front");
+                for (let i = 0; i < colArr.length; i++) {
+                    resultArr.btv.movie.push({
+                        src: colArr[i].children[3].attribs.src,
+                        title: colArr[i].children[3].attribs.alt
+                    });
+                }
+                callback(error, resultArr);
+            });
+        },
+    ], function (err, result) {
+        res.json(result);
+    });
+});
 
 module.exports = router;
